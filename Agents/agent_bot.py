5ab4c12580fbfc00770ab95122bb94b469233408
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-22 18:14:32 +0530

    agent bot file added

---
+++ Agents/agent_bot.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, Lists
+from langchian_core.messages import HumanMessage
+from lanchchai_openai import chatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotend  #used to store secret stuffs like API keys or configuration values.
+
+load_dotenv()  # Load environment variables from a .env file
+
+class AgentState(TypedDict):
+    messages: Lists[HumanMessage]
+
+llm = chatOpenAI(model = "gpt-40")
+
+
+def process(state: AgentState) -> AgentState:
+    response = llm.invoke(state["messages"])
+    print(f"n\nAI: {response.content}")
+    return state
+
+
+graph = StateGraph(AgentState)
+graph.add_node("process," process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END )
+agent = graph.compile()
+
+
+user_input = input("Enter": )
+while user_input ! = "exit":
+  agent.invoke({"messages": [HumanMessage(content=user_input)]})
+  user_input = input("Enter: ")
