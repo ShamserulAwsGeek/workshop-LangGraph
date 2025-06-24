f386903da9983fa0fbaca742f805e577fa171098
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-24 13:14:23 +0530

    react agent added

--- Agents/ReAct.py
+++ Agents/ReAct.py
@@ -39,3 +39,46 @@ def model_call(state: AgentState) -> AgentState:
     system_prompt = SystemMessage( content = "You are my AI assitant, please answer my query to the best of your ability")
     response = model.invoke([system_prompt] + state["messages"])
     return{"messages": [response]}
+
+def should_continue(state:AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else: 
+        return "continue"
+    
+
+graph =StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools = tools)
+graph.add_node("tool_node", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    },
+
+)
+
+graph.add_edge("tools", "our_agent")
+app = graph.compile()
+
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+        
+
+inputs = {"messages":[{("user", "add 40 + 16  and then multiply the result by 4. also tell me a joke please")}]}
+print_stream(app.stream(inputs, stream_mode = "values"))
