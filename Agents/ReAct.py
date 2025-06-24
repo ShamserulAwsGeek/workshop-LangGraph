e27f363310d74af04eee59d7bffbb0c9672eba78
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-24 11:16:28 +0530

    react agent added

--- Agents/ReAct.py
+++ Agents/ReAct.py
@@ -13,3 +13,29 @@ load_dotenv()
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+@tool
+def add(a:int, b: int):
+    """addition function to add two numbers"""
+    return a + b
+
+@tool
+def subtract(a:int, b: int):
+    """subtraction function to subtract two numbers"""
+    return a - b
+
+@tool
+def multiply(a:int, b: int):
+    """multiplication function to multiply two numbers"""
+    return a * b
+
+tools = [add, subtract, multiply]
+
+model = chatOpenAI(model = "gpt-4o").blind_tools(tools)
+
+
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage( content = "You are my AI assitant, please answer my query to the best of your ability")
+    response = model.invoke([system_prompt] + state["messages"])
+    return{"messages": [response]}
