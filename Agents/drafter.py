c3cb649a4b4043a2ba230f212fa4ccbd085a5783
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-24 23:25:22 +0530

    drafter added

--- Agents/drafter.py
+++ Agents/drafter.py
@@ -14,4 +14,35 @@ document_content = ""
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
+
+
+@tool
+def update(content: str) -> str:
+    """update the document with the provided content"""
+    global document_content
+    document_content = content 
+    return f"Document has been updated successfully! The current content is :\n{document_content}"
+
+@tool
+def save(filename: str) -> str:
+    """ save the current document to a text file and finish the process.
+
+    Args:
+        filename: Name for the text file
+    """
+    global document_content 
+
+    if not filename.endswith('.txt'):
+        filename = f"{filename}.txt"
+
+    try: 
+        with open(filename, 'w') as file: 
+            file.write(document_content)
+        print(f"\n document has been saved to: {filename}")
+        return f"Document has been saved to '{filename}' successfully!"
+    except Exception as e:
+        return f"Error saving document: {str(e)}"
     
+    tools =[update, save]
+
+    model = ChatOpenAI(model= "gpt-4o").bind_tools(tools)
