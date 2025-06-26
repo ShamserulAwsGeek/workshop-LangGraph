ff7978fb4d6b53b1573e4d166476c89cc625f886
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-26 19:03:06 +0530

    rag agent added

--- Agents/rag_agent.py
+++ Agents/rag_agent.py
@@ -22,3 +22,30 @@ llm = ChatOpenAI(
 embeddings = OpenAIEmbeddings(
     model = "text-embedding-3-small",
 )
+
+pdf_path = "Stock_Market_Performance_2024.pdf"
+
+# Safety measure I have put for debugging purposes :)
+if not os.path.exists(pdf_path):
+    raise FileNotFoundError(f"PDF file not found: {pdf_path}")
+
+pdf_loader = pyPDFLoader(pdf_path)  # This loads the PDF
+
+# Checks if the PDF is there
+try: 
+    pages = pdf_loader.load()
+    print(f"pdf has been loaded and has {len(pages)} pages")
+
+except Exception as e:
+    print(f"Error loading PDF: {e}")
+    raise
+
+#chunking process:
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  # Size of each chunk
+    chunk_overlap=200,  # Overlap between chunks
+)
+
+pages_split = text_splitter.split_documents(pages) # We now apply this to our pages
+persist_directory = r"C:\mhaque\LangGraph_Book\LangGraphCourse\Agents"  # Directory to persist the database
+collection_name = "rag_collection"  # Name of the collection
