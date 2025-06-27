872fdd8d67537950569b424f8ac583bde4434886
ShamserulAwsGeek <shamserul786@gmail.com>
2025-06-27 21:06:00 +0530

    rag agent added

--- Agents/rag_agent.py
+++ Agents/rag_agent.py
@@ -104,6 +104,32 @@ def should_continue(state: AgentState):
     result = state["messages"][-1]
     return hasattr(result, "tool_calls") and len(result.tool_calls) > 0
 
-    
+
+system_prompt = """
+
+You are an intelligent AI assistant who answers questions about Stock Market Performance in 2024 based on the PDF document loaded into your knowledge base.
+Use the retriever tool available to answer questions about the stock market performance data. You can make multiple calls if needed.
+If you need to look up some information before asking a follow up question, you are allowed to do that!
+Please always cite the specific parts of the documents you use in your answers.
+
+"""
+
+tools_dict = {our_tool.name: our_tool for our_tool in tools}  #creating dictionary of our tools
+
+#LLM Agent
+def call_llm(state: AgentState) -> AgentState:
+    """ Function to call the LLM with the current state"""
+    messages = list(state["messages"])
+    messages = [SystemMessage(content = system_prompt)] + messages
+    messages = llm.invoke(messages)
+    return {'messages' : [messages]}
+
+
+
+
+# Retriever Agent
+
+
+
     
     
